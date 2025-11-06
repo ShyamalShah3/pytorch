cea49f694aa0e41fc2e0e3da153dc69429c0241c
Xiao Fu <xiaofu@meta.com>
2025-11-06 15:28:49 -0800

    [HOP][print] Add functionalization for print
    ghstack-source-id: 821ee4fa9a8fa2cdf695f272aee575e99c747c81
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/167016

--- torch/_higher_order_ops/print.py
+++ torch/_higher_order_ops/print.py
@@ -1,10 +1,13 @@
+from torch.distributed.utils import _unpack_kwargs
 import builtins
 
 import torch
 import torch.utils._pytree as pytree
 from torch._ops import HigherOrderOperator
+from torch._subclasses.fake_tensor import FakeTensorMode
 from torch.fx.experimental.proxy_tensor import ProxyTorchDispatchMode
 
+from typing import Any
 
 class Print(HigherOrderOperator):
     """
@@ -35,6 +38,18 @@ def print_proxy_torch_dispatch_mode(
     proxy_kwargs = pytree.tree_map(mode.tracer.unwrap_proxy, kwargs)  # type: ignore[union-attr]  # noqa: F841
     mode.tracer.create_proxy("call_function", print, (format_str,), proxy_kwargs)
 
+@print.py_functionalize_impl
+# pyre-ignore
+def print_functionalize(ctx: Any, format_str: str, **kwargs: object) -> None:
+    with ctx.redispatch_to_next():
+        res = print_cpu(format_str, **kwargs)
+        return ctx.wrap_tensors(res)
+
+@print.py_impl(FakeTensorMode)
+# pyre-ignore
+def call_delegate_fake_tensor_mode(mode, format_str: str, **kwargs: object):
+    with mode:
+        return print_cpu(format_str, *kwargs)
 
 @print.py_impl(torch._C.DispatchKey.CompositeExplicitAutograd)
 # pyre-ignore
